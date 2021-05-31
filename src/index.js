426df047374bf9e704903b85d7506091881e73fa
VladHlushets <hlushetsvlad@gmail.com>
2021-05-31 18:16:45 +0300

    color

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 import './styles.css';
-import './js/color-swtich.js';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+// Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  // объект ссылок на кнопки
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  bodyRef: document.querySelector('body'),
+};
+
+let intervalId;
+let isActive = false;
+
+refs.startBtn.addEventListener('click', startChangeColor); // слушатели событий по клику
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  if (isActive) {
+    return;
+  }
+
+  intervalId = setInterval(onChangingColor, 1000);
+  isActive = true;
+  refs.startBtn.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  isActive = false;
+  refs.startBtn.disabled = false;
+}
+
+function onChangingColor() {
+  let randomIndexOfElement = randomIntegerFromInterval(0, colors.length - 1);
+  let colorOfBody = colors[randomIndexOfElement];
+  refs.bodyRef.style.backgroundColor = colorOfBody;
+}
